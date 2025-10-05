3eeec2a4b4aff23934942a422b2e1d43d66e576b
parasawas <krapaluawasthi444@gmail.com>
2025-10-05 18:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+const name = document.getElementById("name").innerHTML;
+const year = document.getElementById("year").innerHTML;
+let url = document.getElementById("url");
+const oldURL = url.innerHTML;
+const newURL = oldURL + "?name=" + name + "&year=" + year;
+
+function updateURL() {
+    
+    url.innerHTML = newURL;
+}
+console.log("hiii");
+console.log(oldURL)
+console.log(newURL);
